fcf07fdbb9e1a51a456fa7c02c73263291fee814
DWS-paris <j.noyer@digitalworkshop.fr>
2019-01-17 17:03:12 +0100

    Version

--- 10janv/server.js
+++ 10janv/server.js
@@ -6,12 +6,6 @@ Imports and configuration
     const express = require('express'); // Gestion server
     const path = require('path'); // Gestion du dossier client
     const ejs = require('ejs'); // Gestion du moteur de rendu
-
-    //=> MongoDB
-    const mongoDb = require('./services/db.service');
-
-    // Routes
-    const frontRoutes = require('./routes/front.route');
 //
 
 /* 
@@ -27,23 +21,13 @@ Server config.
     //=> Use path to add views
     server.set( 'views', __dirname + '/www' );
     server.use( express.static(path.join(__dirname, 'www')) );
-
-    //=> Set routes
-    server.use('/', frontRoutes);
 //
 
 /* 
 Launch server
 */
-    //=> Connect MongoDB
-    mongoDb.initConnection()
-    .then( dbResponse => {
-        console.log('MongoDB is connected: ' + dbResponse)
-
-        // Launch server
-        server.listen( port, () => {
-            console.log(`Server is active on port: ${port}`);
-        });
-    })
-    .catch( dbError => console.error(dbError) );
+   // Launch server
+   server.listen( port, () => {
+        console.log(`Server is active on port: ${port}`);
+    });
 //
